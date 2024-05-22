29947a6ce4d3386e5776f2016bc1efb96ed2eb90
Alexis3004 <nelsonalexiscacerzcarreo37@gmail.com>
2024-05-21 23:34:07 -0500

    Se agrega TailwindCss y se ajusta eslint y espacios de indentación

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,13 +2,28 @@
 require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
-  root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-prettier/skip-formatting'
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest'
-  }
+    root: true,
+    extends: [
+        'plugin:vue/vue3-essential',
+        'eslint:recommended',
+        '@vue/eslint-config-prettier/skip-formatting'
+    ],
+    parserOptions: {
+        ecmaVersion: 'latest'
+    },
+    plugins: ['vue'],
+    rules: {
+        indent: ['error', 4],
+        quotes: ['error', 'single', { allowTemplateLiterals: true }],
+        eqeqeq: 'off',
+        'space-before-function-paren': [
+            'error',
+            {
+                // "always", "never", or "ignore"
+                anonymous: 'always', // anonymous is for anonymous function expressions (e.g. function () {}).
+                named: 'ignore', // named is for named function expressions (e.g. function foo () {}).
+                asyncArrow: 'always' // asyncArrow is for async arrow function expressions (e.g. async () => {}).
+            }
+        ]
+    }
 }
--- vite.config.js
+++ vite.config.js
@@ -6,13 +6,19 @@ import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    VueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    plugins: [vue(), VueDevTools()],
+    build: {
+        rollupOptions: {
+            output: {
+                entryFileNames: 'assets/[name]-[hash].js',
+                chunkFileNames: 'assets/[name]-[hash].js',
+                assetFileNames: 'assets/[name]-[hash].[ext]'
+            }
+        }
+    },
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
     }
-  }
 })
