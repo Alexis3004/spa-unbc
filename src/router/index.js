20f8f808f35493da511e6a506674ec9ff0485a81
Alexis3004 <nelsonalexiscacerzcarreo37@gmail.com>
2024-05-22 19:53:08 -0500

    Se cambia método del history del router

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, createWebHashHistory } from 'vue-router'
 import { validateUser, getSlug } from '@/helpers'
 import { useAppStore } from '@/stores/app.js'
 import HomeView from '../views/HomeView.vue'
@@ -164,7 +164,8 @@ const routes = [
 ]
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
+    // history: createWebHistory(import.meta.env.BASE_URL),
+    history: createWebHashHistory(),
     routes,
     scrollBehavior() {
         return { top: 0 }
